1ae86fb873918f519c952b78e9c6acda93a575cb
Barbara  Samira Santiesteban Saravia <145080442+BarbaraSamiraS@users.noreply.github.com>
2025-10-15 00:23:48 -0300

    Update rotas.js

--- controllers/rotas.js
+++ controllers/rotas.js
@@ -1,6 +1,8 @@
 import defineAutenticacao from "./autenticacao/authController.js";
+import defineDashboard from "./dashAdm/dashController.js";
 
 export default function defineRoutes(app, __dirname) {
   // Aqui você só chama as funções de cada módulo
   defineAutenticacao(app, __dirname);
+  defineDashboard(app, __dirname);
 }
